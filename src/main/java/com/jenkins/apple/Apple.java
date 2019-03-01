0694ce4f658dd5d878efa89751dd19582e9c3b09
liulei <liulei13041x@163.com>
2019-03-01 23:22:50 +0800

    change test2

--- src/main/java/com/jenkins/apple/Apple.java
+++ src/main/java/com/jenkins/apple/Apple.java
@@ -1,7 +1,7 @@
 package com.jenkins.apple;
 
 public class Apple {
-    private String apple = "first test1";
+    private String apple = "first test2";
 
     public String getApple() {
         return apple;
